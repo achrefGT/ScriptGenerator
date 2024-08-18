80aa4e9b98403c2597f24998d2e5f85601488c3c
achrefGT <achrefgataa@gmail.com>
2024-08-18 14:31:20 +0100

    Co-trans scenario handling revised

--- main/urls.py
+++ main/urls.py
@@ -4,11 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path('CreateLLD/', views.create_LLD, name='create_LLD'),
-    path('CreateRouter/', views.create_router, name='create_router'),
-    path('CreateRadioSite/', views.create_radioSite, name='create_radioSite'),
     path('upload/', views.upload_lld, name='upload_lld'),
     path('uploadCoTrans/', views.upload_lld_Co_Trans, name='upload_lld_Co_Trans'),
-    path('success/', views.success_view, name='file_upload_success'), 
     path('download-script/', views.download_script, name='download_script'),
 ]
--- main/views.py
+++ main/views.py
@@ -2,150 +2,9 @@ from django.shortcuts import render
 from django.views.decorators.http import require_POST
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
-from django.forms import inlineformset_factory
 from .models import Router, PhysicalInterface, Interface2G, Interface3G, Interface4G, ManagementInterface, Script, LowLevelDesign, LowLevelDesign_Co_trans,RadioSite
-from .forms import (
-    LowLevelDesignForm,
-    RouterForm,
-    RadioSiteForm,
-    PhysicalInterfaceFormSet,
-    Interface2GFormSet,
-    Interface3GFormSet,
-    Interface4GFormSet,
-    ManagementInterfaceFormSet,
-)
+from .forms import LowLevelDesignForm
 import pandas as pd
-
-def index(request):
-    return render(request, "main/base.html")
-
-def create_radioSite(request):
-    if request.method == "POST" :
-        radio_site_form = RadioSiteForm(request.POST)
-        if radio_site_form.is_valid():
-            # Save the RadioSite instance
-            radio_site = radio_site_form.save(commit=False)
-            radio_site.save()
-        return redirect('index')
-    else :
-        radio_site_form = RadioSiteForm()
-    return render(request, "main/create_radioSite.html", {
-        'radio_site_form': radio_site_form
-        })
-
-def create_LLD(request):
-    if request.method == "POST" :
-        low_level_design_form = LowLevelDesignForm(request.POST)
-        if low_level_design_form.is_valid():
-            low_level_design = low_level_design_form.save(commit=False)
-            low_level_design.save()
-        return redirect('index')
-    else :
-        low_level_design_form = LowLevelDesignForm()
-    return render(request, "main/create_LLD.html", {
-        'low_level_design_form': low_level_design_form
-        })
-
-
-def create_router(request):
-    if request.method == 'POST':
-        # Instantiate all forms with POST data
-        router_form = RouterForm(request.POST)
-        physical_interface_formset = PhysicalInterfaceFormSet(request.POST)
-        interface2g_formset = Interface2GFormSet(request.POST)
-        interface3g_formset = Interface3GFormSet(request.POST)
-        interface4g_formset = Interface4GFormSet(request.POST)
-        management_interface_formset = ManagementInterfaceFormSet(request.POST)
-
-        # Validate all forms and formsets
-        if (router_form.is_valid() and
-            physical_interface_formset.is_valid() and
-            interface2g_formset.is_valid() and
-            interface3g_formset.is_valid() and
-            interface4g_formset.is_valid() and
-            management_interface_formset.is_valid()):
-
-            # Save the Router instance
-            router = router_form.save()
-
-            # Save physical interfaces and associate them with the router
-            physical_interfaces = physical_interface_formset.save(commit=False)
-            for physical_interface in physical_interfaces:
-                physical_interface.router = router
-                physical_interface.save()
-
-                # Save logical interfaces associated with the physical interface
-                for formset in [interface2g_formset, interface3g_formset, interface4g_formset, management_interface_formset]:
-                    logical_interfaces = formset.save(commit=False)
-                    for logical_interface in logical_interfaces:
-                        logical_interface.physicalInterface = physical_interface
-                        logical_interface.save()
-
-            # Generate the script after saving all the data
-            lld = router.lld  # Assuming the Router has a ForeignKey to LowLevelDesign
-            script = lld.generateScript()
-
-            # Pass the script content to the result template
-            return render(request, 'main/result.html', {'script_content': script.content})
-
-    else:
-        # Instantiate all forms with empty data
-        router_form = RouterForm()
-        physical_interface_formset = PhysicalInterfaceFormSet(queryset=PhysicalInterface.objects.none())
-        interface2g_formset = Interface2GFormSet(queryset=Interface2G.objects.none())
-        interface3g_formset = Interface3GFormSet(queryset=Interface3G.objects.none())
-        interface4g_formset = Interface4GFormSet(queryset=Interface4G.objects.none())
-        management_interface_formset = ManagementInterfaceFormSet(queryset=ManagementInterface.objects.none())
-
-    return render(request, "main/create_router.html", {
-        'router_form': router_form,
-        'physical_interface_formset': physical_interface_formset,
-        'interface2g_formset': interface2g_formset,
-        'interface3g_formset': interface3g_formset,
-        'interface4g_formset': interface4g_formset,
-        'management_interface_formset': management_interface_formset,
-    })
-
-
-def upload_file(request):
-    if request.method == 'POST':
-        form = LowLevelDesignForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-            return redirect('file_upload_success')
-    else:
-        form = LowLevelDesignForm()
-    return render(request, 'main/uploadLLD.html', {'form': form})
-
-
-def success_view(request):
-    return render(request, 'main/success.html')
-
-
-@require_POST
-def download_script(request):
-    script_content = request.POST.get('script_content', '')  
-    
-    response = HttpResponse(script_content, content_type='text/plain')
-    response['Content-Disposition'] = 'attachment; filename="script.txt"'
-
-    return response
-
-
-def get_column_value(row, column_name, column_index, sheet_name):
-    """
-    Try to get the value by column name; if it fails, use the column index.
-    If both fail, raise an error with details about the column and sheet.
-    """
-    try:
-        return row[column_name]
-    except KeyError:
-        result = row[column_index]
-        if not pd.isna(result):  
-            return result
-    
-        raise ValueError(f"Failed to find the column '{column_name}' in sheet '{sheet_name}'.")
-
 
 
 IP_PLAN_COLUMNS = {
@@ -185,10 +44,40 @@ INTERFACE_4G_COLUMNS = {
 
 LLD_CO_TRANS_COLUMNS = {
     'NE40/NE8000': 0,
-    'site ': 1,
+    'site  ': 1,
     'Config O&M': 2,
     'Config TDD': 3,
 }
+
+def index(request):
+    return render(request, "main/base.html")
+
+
+@require_POST
+def download_script(request):
+    script_content = request.POST.get('script_content', '')  
+    
+    response = HttpResponse(script_content, content_type='text/plain')
+    response['Content-Disposition'] = 'attachment; filename="script.txt"'
+
+    return response
+
+
+def get_column_value(row, column_name, column_index, sheet_name):
+    """
+    Try to get the value by column name; if it fails, use the column index.
+    If both fail, raise an error with details about the column and sheet.
+    """
+    try:
+        return row[column_name]
+    except KeyError:
+        result = row[column_index]
+        if not pd.isna(result):  
+            return result
+    
+        raise ValueError(f"Failed to find the column '{column_name}' in sheet '{sheet_name}'.")
+
+
 
 def upload_lld(request):
     if request.method == 'POST':
@@ -292,7 +181,7 @@ def upload_lld(request):
 
             except Exception as e:
                 # Handle any errors that occur
-                error_message = f"An error occurred while processing the file: {str(e)}"
+                error_message = f"An error occurred while processing the file, please check your LLD"
                 return render(request, 'main/uploadLLD.html', {'form': form, 'error': error_message})
 
 
@@ -310,19 +199,18 @@ def upload_lld_Co_Trans(request):
             try:
                 excel_file = request.FILES['file']
                 lld_data_df = pd.read_excel(excel_file, sheet_name=0)  
-                print(lld_data_df)
                 # Create LowLevelDesign instance
                 lld = LowLevelDesign_Co_trans.objects.create(file=excel_file)  
 
 
                 # Process the first row of the DataFrame (assuming it contains the data)
                 for _, row in lld_data_df[1:].iterrows():
-                    router_name = row[0]
-                    site_name = row[1]
-                    o_and_m_ip = row[2]  
-                    tdd_ip = row[3] 
 
-                    print(f" ******************************** {router_name} / {site_name} :{o_and_m_ip} + {tdd_ip}")
+                    router_name = get_column_value(row, 'NE40/NE8000', LLD_CO_TRANS_COLUMNS['NE40/NE8000'], 'Sheet1')
+                    site_name = get_column_value(row, 'site  ', LLD_CO_TRANS_COLUMNS['site  '], 'Sheet1')
+                    o_and_m_ip = get_column_value(row, 'Config O&M', LLD_CO_TRANS_COLUMNS['Config O&M'], 'Sheet1')
+                    tdd_ip = get_column_value(row, 'Config TDD', LLD_CO_TRANS_COLUMNS['Config TDD'], 'Sheet1')
+
 
 
                     if router_name and site_name:
@@ -337,7 +225,7 @@ def upload_lld_Co_Trans(request):
                 return render(request, 'main/result.html', {'script_content': script.content})
 
             except Exception as e:
-                error_message = f"An error occurred while processing the file: {str(e)}"
+                error_message = f"An error occurred while processing the file, please check your LLD"
                 return render(request, 'main/uploadLLD.html', {'form': form, 'error': error_message})
 
     else:
